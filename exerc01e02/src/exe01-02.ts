ac7a653f7d1b8b0d4a2c6944ee6cb68a26c87214
Clovis Garcia <theviolatorx@gmail.com>
2023-08-29 22:00:48 -0300

    atualizando a estrutura de diretórios para adicionar os novos exercícios separando por pastas.

--- exerc01e02/src/exe01-02.ts
+++ exerc01e02/src/exe01-02.ts
@@ -1,12 +1,20 @@
-"use strict";
 /* 1. Crie uma classe chamada `Person` com propriedades `name` e `age`. Instancie alguns objetos dessa classe.
 2. Adicione um método à classe `Person` que imprime uma saudação com o nome da pessoa.
 */
 class Person {
-    constructor(name, age) {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number){
         this.name = name;
         this.age = age;
     }
+
+    hello(): string {
+        return `Olá ${this.name}! Você esta bem para os seus ${this.age} anos de idade!`;
+    }
 }
-const pessoa = new Person("Clóvis Garcia", 48);
+
+const pessoa = new Person("Clóvis Garcia",48);
 console.log(pessoa);
+console.log(pessoa.hello());
